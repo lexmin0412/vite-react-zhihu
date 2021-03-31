44f7ad3f59455f72356a51cf73b449efe2956b1b
lexmin0412 <zhangle_media@hotmail.com>
2021-03-31 23:25:34 +0800

    feat: 引入react-loading, 加载体验优化

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react'
 import Tabbar from './../../components/tabbar'
 import HomeService from './../../services/zhihu/home'
 import styles from './index.module.css'
+import ReactLoading from 'react-loading';
 
 export default function HomeIndex() {
 
 	const [ list, setList ] = useState<Array<any>>([])
+	const [ fetchFinished, setFetchFinished ] = useState<Boolean>(false)
 
 	useEffect(() => {
 		HomeService.getRecommendList().then((res: any)=>{
 			console.log('请求结果', res)
 			setList(res)
+			setFetchFinished(true)
 		})
 	}, []);
 
@@ -18,36 +21,45 @@ export default function HomeIndex() {
 		<div className={styles.homeIndexPage}>
 			<div className={styles.listContainer}>
 				{
-					list.map((item: any)=>{
-						return (
-							<div className={styles.listItem}
-								key={item.id}
-							>
-								<div className={styles.itemTop}>
-									<img className={styles.itemHeadImg} src={item.headimg} />
-									<div className={styles.itemUserInfo}>
-										<div className={styles.itemUserName}>
-											{item.user}
+					fetchFinished ?
+					<React.Fragment>
+							{
+								list.map((item: any) => {
+									return (
+										<div className={styles.listItem}
+											key={item.id}
+										>
+											<div className={styles.itemTop}>
+												<img className={styles.itemHeadImg} src={item.headimg} />
+												<div className={styles.itemUserInfo}>
+													<div className={styles.itemUserName}>
+														{item.user}
+													</div>
+													<div className={styles.itemUpdateTime}>
+														{item.timeStamp}
+													</div>
+												</div>
+											</div>
+											<div className={styles.itemMain}>
+												<div className={styles.title}>
+													{item.title}
+												</div>
+												<div className={styles.content}>
+													<div className={styles.desc}>
+														{item.desc}
+													</div>
+													<img src={item.mainPic} alt="" className={styles.mainPic} />
+												</div>
+											</div>
 										</div>
-										<div className={styles.itemUpdateTime}>
-											{item.timeStamp}
-										</div>
-									</div>
-								</div>
-								<div className={styles.itemMain}>
-									<div className={styles.title}>
-										{item.title}
-									</div>
-									<div className={styles.content}>
-										<div className={styles.desc}>
-											{item.desc}
-										</div>
-										<img src={item.mainPic} alt="" className={styles.mainPic}/>
-									</div>
-								</div>
-							</div>
-						)
-					})
+									)
+								})
+							}
+					</React.Fragment>
+						:
+						<div className={`${styles.flex} ${styles.fill}`}>
+							<ReactLoading type='spokes' color='#06f' height={40} width={40} />
+						</div>
 				}
 			</div>
 			<Tabbar />
