b24fe4609247cbb5e5853d8afda7985dcb103053
lexmin0412 <zhangle_media@hotmail.com>
2021-04-04 01:30:14 +0800

    test: delete test code

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,17 +1,8 @@
 import React, {useEffect} from 'react'
 import Tabbar from './../../components/tabbar'
 import styles from './user.module.css'
-import Request from './../../utils/request'
 
 export default function UserIndex() {
-
-	useEffect(()=>{
-		Request({
-			url: `${process.env.VITE_NETEASE_API}/topic/detail?actid=111551188`
-		}).then((res)=>{
-			console.log('请求结果', res)
-		})
-	}, [])
 
 	return (
 		<div className={styles.userIndexPage}>
@@ -21,9 +12,6 @@ export default function UserIndex() {
 					<div className={styles.userInfoAbbr}>
 						<div className="userName">
 							Lexmin
-						</div>
-						<div className="userPump">
-							打气值
 						</div>
 					</div>
 				</div>
